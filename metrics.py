73b38193f68ea1707acd6faf6e7a62b75b24bcc1
YuAn-06 <62364010+YuAn-06@users.noreply.github.com>
2022-01-28 10:50:17 +0800

    The metrics of model

---
+++ metrics.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn.metrics import mean_squared_error,mean_absolute_error
+
+def RMSE(y_test,y_pred):
+
+    rmse = mean_squared_error(y_true=y_test,y_pred=y_pred)
+    return np.sqrt(rmse)
+
+def MAE(y_test,y_pred):
+    mae = mean_absolute_error(y_true=y_test,y_pred=y_pred)
+    return mae
